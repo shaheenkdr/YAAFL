e2f3b2c6784eb8b4546877dba60212097fcfd136
shaheenkdr <shaheenkader@outlook.com>
2016-09-28 01:37:31 -0700

    Added homeAway neuron and corresponding event bus

--- app/src/main/java/com/udacity/yaafl/SplashActivity.java
+++ app/src/main/java/com/udacity/yaafl/SplashActivity.java
@@ -2,7 +2,11 @@ package com.udacity.yaafl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.crashlytics.android.Crashlytics;
+import com.udacity.yaafl.neuron.HomeAway;
+
 import io.fabric.sdk.android.Fabric;
 
 public class SplashActivity extends AppCompatActivity {
@@ -12,5 +16,8 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_splash);
+
+        HomeAway h1 = new HomeAway(0,true);
+        Log.e("Thenga",""+h1.getChances());
     }
 }
